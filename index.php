9b6e6f8c414732e487e9a44cd87db29e3bd1bf75
Gustavo Henrique Fontes Augusto <gustavogvfontes2004@gmail.com>
2024-10-15 15:42:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,44 @@
     <title>Meu Portifolio</title>
 </head>
 <body>
-    <h1>
-        <?php 
-        $saudacao = 'olá';
-            echo $saudacao . ", portifolio do gustavo";
-        ?>
-    </h1>
-    <p>
-        <?php
-        echo "olá, mundo"
-        ?>
-    </p>
+    <?php 
+    $nome = "Gustavo";
+    $saldacao = "olá";
+    $titulo = $saldacao . " Portifolio do " . $nome;
+    $subtitulo = "seja bem vindo no meu portifolio";
+
+    $ano = "2024";    
+
+    $projeto = "Meu portifolio";
+    $finalizado = true;
+    $dataDoProjeto = "2024-10-15";
+    $descricao = "Meu primeiro portifolio"
+    ?>
+
+    <h1><?=$titulo?></h1>
+    <p><?=$subtitulo?></p>
+    <h1><?=$ano?></h1>
+    
+</hr>
+    <div>
+        <h2></=$projeto?></h2>
+        <p><?=$descricao?></p>
+        <div><?=$dataDoProjeto?></div>
+
+        <div> Projeto
+            <?php if($finalizado): ?>
+                <span style="color: green">✅Finalizado</span>
+
+            <?php else: ?>
+                <span style="color: red">⛔Não Finalizado</span>
+
+                <?php endif; ?>
+
+
+
+        </div>
+
+    </div>
     
     
 
